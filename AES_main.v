97bc355a58ffb1b520dfefcd22ce1359937baf96
mohamed yasser <m.yasser.2004.my@gmail.com>
2024-05-12 18:53:21 +0300

    commit

--- AES_main.v
+++ AES_main.v
@@ -1,30 +1,23 @@
- module AES_main #(parameter Nk =4, parameter Nr = 10) (clk,reset ,Seg1 ,Seg2 , Seg3 ,LED);
+ module AES_N #(parameter Nk =4, parameter Nr = 10) (clk,reset,key ,LED , seg_DE);
  input clk;
  input reset;
-   output[6:0] Seg1;
-   output[6:0] Seg2;
-   output[6:0] Seg3;
+ input [32*Nk-1:0]key;
    output reg LED;
+   output reg [127:0]  seg_DE ;
    wire clks=clk;
    wire resets=reset;
 
    wire [127:0] pl =128'h00112233445566778899aabbccddeeff;
 
-   wire [127:0] ky1 =128'h000102030405060708090a0b0c0d0e0f;
-   wire [191:0] ky2= 192'h000102030405060708090a0b0c0d0e0f1011121314151617;
-   wire [255:0] ky3= 256'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f; 
-
    wire [127:0] cOut;
    wire [127:0] DecOut;
-   reg [127:0]  seg_DE ;
    reg [127:0] inpD;
    reg [4:0] Count = 5'd0;
 
 
-	  Showing_LS_Byte  S (clks , seg_DE[7:0] , Seg1 , Seg2 , Seg3);
 
-   cipherEN #(Nk ,Nr)E (clks,resets,pl,cOut,ky1);
-   decipherDE #(Nk,Nr)D (clks,resets,cOut,DecOut,ky1);
+   cipherEN #(Nk ,Nr)E (clks,resets,pl,cOut,key);
+   decipherDE #(Nk,Nr)D (clks,resets,cOut,DecOut,key);
               
    
 
@@ -48,7 +41,7 @@
 		  else if(Count>=(2*(Nr+1)+1)) begin 
 		    seg_DE[7:0]=pl[7:0];
         
-		  LED=1'b1;;
+		  LED=1'b1;
 		end
 
     end
@@ -89,46 +82,49 @@
    endmodule
 
 
-module AES (
+module AES_main (
     input clk,
     input reset,
     input Mode1,
     input Mode2,
     input Mode3,
-    output reg [6:0] Seg1,
-    output reg [6:0] Seg2,
-    output reg [6:0] Seg3,
+    output  [6:0] Seg1,
+    output  [6:0] Seg2,
+    output  [6:0] Seg3,
     output reg  LED
 );
 
-wire [6:0] Seg1_temp, Seg2_temp, Seg3_temp; // Temporary variables for AES_N outputs
-wire L;
-// Instantiate AES_N modules outside always block
-AES_N #(.Nk(4), .Nr(10)) e1(clk, reset, Seg1_temp, Seg2_temp, Seg3_temp, L);
-AES_N #(.Nk(6), .Nr(12)) e2(clk, reset, Seg1_temp, Seg2_temp, Seg3_temp, L);
-AES_N #(.Nk(8), .Nr(14)) e3(clk, reset, Seg1_temp, Seg2_temp, Seg3_temp, L);
+wire [127:0]  OutPut1 , OutPut2 , OutPut3;
+reg[127:0] TheOut;
 
+reg L;
+wire L1, L2 , L3;
+
+wire [127:0] ky1 =128'h000102030405060708090a0b0c0d0e0f;
+wire [191:0] ky2= 192'h000102030405060708090a0b0c0d0e0f1011121314151617;
+wire [255:0] ky3= 256'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f; 
+
+AES_N #(.Nk(4), .Nr(10)) e1(clk, reset, ky1,L1 , OutPut1);
+AES_N #(.Nk(6), .Nr(12)) e2(clk, reset,ky2 ,L2 , OutPut2);
+AES_N #(.Nk(8), .Nr(14)) e3(clk, reset,  ky3,L3 , OutPut3);
+
+	  Showing_LS_Byte  S (clks , TheOut , Seg1 , Seg2 , Seg3);
 
 // Assign outputs based on mode selection
-always @(posedge clk) begin
+always @(*) begin
     if (reset) begin
-        Seg1 <= 7'b0;
-        Seg2 <= 7'b0;
-        Seg3 <= 7'b0;
-        LED <= 1'b0;
+        TheOut=128'h0;
+		  L=1'b0;
     end else begin
         if (Mode1) begin
-            Seg1 <= Seg1_temp;
-            Seg2 <= Seg2_temp;
-            Seg3 <= Seg3_temp;
+            TheOut=OutPut1;
+				L=L1;
         end else if (Mode2) begin
-            Seg1 <= Seg1_temp;
-            Seg2 <= Seg2_temp;
-            Seg3 <= Seg3_temp;
+                        TheOut=OutPut2;
+L=L2;
         end else if (Mode3) begin
-            Seg1 <= Seg1_temp;
-            Seg2 <= Seg2_temp;
-            Seg3 <= Seg3_temp;
+                        TheOut=OutPut3;
+L=L3;
         end
         // You may want to add an else condition for undefined behavior
     end
